d8dfe739044aed31a0263c481d5e9980704563d6
Ethan Gould <ethan.g.907@gmail.com>
2016-10-16 17:07:34 -0400

    added cool drop on animation for new song additions

--- app.js
+++ app.js
@@ -1,19 +1,45 @@
 $(document).ready(function(){
-	$('.new-song').submit(function(event) {
+
+	var module = {};
+
+	module.init = function() {
+		module.songList = $('.new-song__list');
+		module.eventHandlers();
+	};
+
+
+	module.eventHandlers = function() {
+		$('.new-song__form').submit(module.parseSongInput);
+	};
+
+	module.buildSongEntry = function(song) {
+		var songMarkup = 	'<li class="new-song__result js-song-result">' +
+												'<p class="new-song__entry"><span class="new-song__title js-song-title">' + song.title + '</span><span class="new-song__artist js-song-artist">' + song.artist + '</span></p>' +
+											'</li>';
+
+		module.songList.append(songMarkup);
+		setTimeout(function() {
+			$('.js-song-result').addClass('drop-show');
+		},10);
+	};
+
+	module.parseSongInput = function(event) {
 		event.preventDefault();
-		var input = $(this).find('input.parsable').val().split('/');
+		var $input = $(this).find('input[type="text"]');
+		var values = $input.val().split('/');
 
 		var newSong = {
-			title: input[0],
-			artist: input[1]
+			title: values[0] || '',
+			artist: values[1] || ''
 		};
 
-		if (input.length[2]) {
-			newSong.link = input[2];
+		if (values.length[2]) {
+			newSong.link = values[2];
 		}
-		
-		$('.song .title').text(newSong.title);;
-		$('.song .artist').text(newSong.artist);
-		$('.song').show();
-	});
+
+		module.buildSongEntry(newSong);
+		$input.val('');
+	}
+
+	module.init();
 });
